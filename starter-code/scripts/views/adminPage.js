09e88ddab8d9c0ee143ba6d400cb64ad70632363
Scott Schmidt <sjschmidt44@gmail.com>
2016-12-01 18:37:52 -0800

    starter code for class 07

---
+++ starter-code/scripts/views/adminPage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+articleView.renderAdminPage = function() {
+  var statsRender = Handlebars.compile($('#stats-template').html());
+  Article.numWordsByAuthor().forEach(function(numWordsObj) {
+    $('.author-stats').append(statsRender(numWordsObj));
+  });
+
+  $('#blog-stats .articles').text(Article.allArticles.length);
+  $('#blog-stats .words').text(Article.numWordsAll());
+};
+
+Article.fetchAll(articleView.renderAdminPage);
